9895efbf5c2e7bcfa40200477d37b4f963a9801e
Nenad Mikša <nenad.miksa@microblink.com>
2023-06-15 13:46:53 +0200

    handle libc++

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,17 +1,22 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout, CMake
-import os
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "build_type", "arch", "compiler"
-    generators = "CMakeToolchain", "VCVars"
+    generators = "VCVars"
 
     def requirements(self):
         self.tool_requires(self.tested_reference_str)
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        cmake = CMakeToolchain(self)
+        if self.settings.compiler.libcxx == 'libc++v2':
+            cmake.cache_variables['SHOULD_USE_LIBCXX'] = True
+        cmake.generate()
 
     def build(self):
         cmake = CMake(self)
